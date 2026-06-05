99e42ea50728af5a541dcab54e42daff3e6c503e
Pankaj Singh <jaat548986@gmail.com>
2026-06-05 16:09:53 +0530

    Pattern no 12 is completed

---
+++ Pattern/1.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+using namespace std;
+
+void print1(int n){
+   for(int i=0; i<n;i++){
+        for(int j=0;j<n;j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void print2 (int n){
+    for(int i=0; i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+void print3 (int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << j+1 << " ";
+        }
+        cout << endl;
+    }
+}
+void print4 (int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << i+1 << " ";
+        }
+        cout << endl;
+    }
+}
+void print5 (int n){
+    for(int i=0;i<n;i++){
+        for(int j=n;j>i;j--){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+void print6 (int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<(n-i);j++){
+            cout << j+1 << " ";
+        }
+        cout << endl;
+    }
+}
+void print7 (int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            cout << " ";
+        }
+        for(int k=0;k<(2*i+1);k++){
+            cout << "*";
+        }
+        for(int l=0;l<n-i;l++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void print8 (int n){
+    for(int i=0;i<n;i++){
+        for(int j=n;j>=n-i;j--){
+            cout << " ";
+        }
+        for(int j=0;j<(2*(n-i)-1);j++){
+            cout << "*";
+        }
+        for(int j=n;j>=n-i;j--){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+// long way
+void print9 (int n){
+    for(int i=1;i<=(2*n-1);i++){
+        if(i<=n){
+        for(int j=0;j<i;j++){
+            cout << "* ";
+        }
+    }
+        if(i>n){
+            for(int j=i-n;j<n;j++){
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+// short way
+
+void print10 (int n){
+    for(int i=1; i<=(2*n-1);i++){
+        int stars =i;
+        if(i>n){
+            stars=(2*n-i);
+        }
+        for(int j=1; j<=stars;j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+
+void print11(int n){
+    int start=1;
+  for(int i=0;i<n;i++){
+    if(i%2==0) {start=1;}
+    else {start=0;}
+    for(int j=0; j<=i;j++){
+        cout << start;
+        start=1-start;
+    }
+    cout << endl;
+  }
+}
+
+int main(){
+    int num;
+    cin >> num;
+
+    print11(num);
+    
+    return 0;
+}
