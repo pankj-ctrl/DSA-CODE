f601cb7f8d95eb55e3dff56efd634b2b2d9d4433
Pankaj Singh <jaat548986@gmail.com>
2026-06-06 09:12:20 +0530

    Pattern 12

--- Pattern/1.cpp
+++ Pattern/1.cpp
@@ -124,11 +124,26 @@ void print11(int n){
   }
 }
 
+void print12 (int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << j+1;
+        }
+        for(int j=0;j<(2*(n-i)-2);j++){
+            cout << " ";
+        }
+        for(int j=i; j>=0;j--){
+            cout << j+1;
+        }
+        cout << endl;
+    }
+}
+
 int main(){
     int num;
     cin >> num;
 
-    print11(num);
+    print12(num);
     
     return 0;
 }
