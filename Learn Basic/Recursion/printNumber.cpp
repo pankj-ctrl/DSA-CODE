8c65c49bac4e2eb62c03689ed3a3aaf81a962c74
Pankaj Singh <jaat548986@gmail.com>
2026-06-07 11:22:37 +0530

    Print Number 1 to N using Recursion

---
+++ Learn Basic/Recursion/printNumber.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printNum(int i ,int n){
+    if(i>n){
+        return;
+    }
+    cout << i <<endl;
+    printNum(i+1,n);
+}
+
+int main (){
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+
+    printNum(1,n);
+    return 0;
+}
