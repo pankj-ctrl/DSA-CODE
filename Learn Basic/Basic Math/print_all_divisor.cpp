6555c3676244d9e4e84512c4e97761902cc74b40
Pankaj Singh <jaat548986@gmail.com>
2026-06-07 09:14:20 +0530

    optimized way of solving the print all divisor problem

--- Learn Basic/Basic Math/print_all_divisor.cpp
+++ Learn Basic/Basic Math/print_all_divisor.cpp
@@ -1,15 +1,38 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     int num;
     cout << "Enter a number : ";
     cin >> num;
+
+    /*
+    it takes O(n) time for ruuning
     for(int i=1; i<=num;i++){
         if(num%i==0){
             cout << i << " ";
         }
     }
+    cout << endl;
+    
+    */
+    
+    vector<int> ls;
+
+    // Optimized way 
+    for(int i=1; i<=sqrt(num);i++){
+        if(num%i==0){
+        ls.push_back(i);
+
+        if((num/i) != i){
+            ls.push_back(num/i);
+        }
+        }
+        
+    }
+    sort(ls.begin(),ls.end());
+
+    for(auto it: ls)cout<< it << " ";
     cout << endl;
     return 0;
 }
