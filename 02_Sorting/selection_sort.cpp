1b7b8c9624180f2ccb6c2f5b9292cd0d442ab9bc
Pankaj Singh <jaat548986@gmail.com>
2026-06-08 12:20:03 +0530

    Selection sort code

---
+++ 02_Sorting/selection_sort.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[],int n){
+    for(int i=0;i<=n-2;i++){
+        int mini=i;
+        for(int j=i;j<n;j++){
+            if(arr[mini]>arr[j]){
+                mini=j;
+            }
+        }
+        int temp=arr[mini];
+        arr[mini]=arr[i];
+        arr[i]=temp;
+
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    selection_sort(arr,n);
+
+     for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
