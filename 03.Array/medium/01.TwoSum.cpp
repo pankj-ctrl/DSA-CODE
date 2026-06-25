32794986a66521c66201af5dc77be1401915a9ca
Pankaj Singh <jaat548986@gmail.com>
2026-06-25 10:59:14 +0530

    Better

--- 03.Array/medium/01.TwoSum.cpp
+++ 03.Array/medium/01.TwoSum.cpp
@@ -1,6 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+First, we will use a loop (say i) to select the indices of the array one by one.
+For every index i, we will traverse through the remaining array using another loop (say j) to find the other number such that the sum is equal to the target (i.e. arr[i] + arr[j] = target).
+Observation: In every iteration, if the inner loop starts from index 0, we will be checking the same pair of numbers multiple times. For example, in iteration 1, for i = 0, we will check for the pair arr[0] and arr[1]. Again in iteration 2, for i = 1, we will check arr[1] and arr[0]. So, to eliminate these same pairs, we will start the inner loop from i + 1.
+
+*/
+
 pair<int, int> bruteForce(int arr[],int n,int target){
     for(int i=0;i<n;i++){
         int tem=arr[i];
@@ -14,6 +21,24 @@ pair<int, int> bruteForce(int arr[],int n,int target){
   return {-1,-1};
 }
 
+/*
+Here i use Hash Map apporach
+*/
+
+pair <int ,int > better(int arr[],int n , int target){
+      map <int, int > mpp;
+      for(int i=0;i<n;i++){
+        int a =arr[i];
+        int more = target-a;
+        if(mpp.find(more) != mpp.end()){
+            return {mpp[more],i};
+        }
+        mpp[a]=i;
+      }
+      return {-1,-1};
+}
+
+
 int main(){
     int n,target;
     cout << "Welcome To Two Sum Problem: " << endl;
@@ -26,7 +51,7 @@ int main(){
         cin >> arr[i];
     }
 
-    pair <int ,int> result =bruteForce(arr,n,target);
+    pair <int ,int> result =better(arr,n,target);
 
     cout << result.first << "  " << result.second << endl;
     return 0;
