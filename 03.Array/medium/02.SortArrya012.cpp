b5627161e30f58cd112bc20086dcc8d842644700
Pankaj Singh <jaat548986@gmail.com>
2026-06-26 09:30:12 +0530

    Better solution

---
+++ 03.Array/medium/02.SortArrya012.cpp
@@ -0,0 +1,45 @@
+// Sort an array of 0s, 1s and 2s
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ void better(int arr[],int n){
+   int count0 = 0, count1 = 0, count2 = 0;
+   for(int i=0;i<n;i++){
+    if(arr[i]==0){
+        count0++;
+    }else if(arr[i]==1){
+        count1++;
+    }else{
+        count2++;
+    }
+   }
+   for(int i=0;i<count0;i++){
+     arr[i]=0;
+   }
+   for(int i=count0;i<count0+count1;i++){
+    arr[i]=1;
+   }
+   for(int i=count0+count1;i<n;i++){
+    arr[i]=2;
+   }
+ }
+int main(){
+    int n;
+    cout << "Enter the size of Array : ";
+    cin >> n;
+    int arr[n];
+    cout << "Now enter the Element: \n";
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    better(arr,n);
+
+    for(int i=0;i<n;i++){
+        cout << arr[i] << "  ";
+    }
+    cout << endl;
+    return 0;
+}
